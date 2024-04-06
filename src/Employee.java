8fb73916773ee86f7ccc230a371e21eef741dd9c
Vladislav Zinziuk <2962266@mail.ru>
2024-04-06 15:51:10 +0300

    basic level

---
+++ src/Employee.java
@@ -0,0 +1,60 @@
+import java.util.Objects;
+
+public class Employee {
+    private final String fullName;
+    private int department;
+    private int salary;
+    private final int id;
+    private static int counter;
+
+    public Employee(String fullName, int department, int salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++counter;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "fullName=" + fullName +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && fullName.equals(employee.fullName);
+    }
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,72 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final int AMOUNT_EMPLOYEE = 10;
+
     public static void main(String[] args) {
-        System.out.println();
+        final Employee[] employees = new Employee[AMOUNT_EMPLOYEE];
+
+        employees[0] = new Employee("John",1,15000);
+        employees[1] = new Employee("John1",2,11000);
+        employees[2] = new Employee("John2",4,12000);
+        employees[3] = new Employee("John3",5,13000);
+        employees[4] = new Employee("John4",2,14000);
+        employees[5] = new Employee("John5",3,16000);
+        employees[6] = new Employee("John6",5,17000);
+        employees[7] = new Employee("John7",1,19000);
+        employees[8] = new Employee("John8",2,29000);
+        employees[9] = new Employee("John9",3,42000);
+
+        printEmployees(employees);
+        printFullNames(employees);
+
+        System.out.println("Суммарная зарплата = "+ getSalarySum(employees));
+        System.out.println("Минимальная зарплата = "+ getMinSalary(employees));
+        System.out.println("Максимальная зарплата = "+ getMaxSalary(employees));
+        System.out.println("Средняя зарплата = "+ getAverageSalary(employees));
+
     }
+
+    private static void printEmployees(Employee[] employees){
+        for (int i = 0; i < employees.length; i++){
+            System.out.println(employees[i]);
+        }
+    }
+    private static int getSalarySum(Employee[] employees){
+        int salarySum = 0;
+        for (Employee employee : employees) {
+            salarySum += employee.getSalary();
+        }
+        return salarySum;
+    }
+    private static int getMinSalary(Employee[] employees){
+        int minSalary = employees[0].getSalary();
+        for (Employee employee : employees) {
+            if (employee.getSalary() < minSalary){
+                minSalary = employee.getSalary();
+            }
+        }
+        return minSalary;
+    }
+    private static int getMaxSalary(Employee[] employees){
+        int maxSalary = employees[0].getSalary();
+        for (Employee employee : employees) {
+            if (employee.getSalary() > maxSalary){
+                maxSalary = employee.getSalary();
+            }
+        }
+        return maxSalary;
+    }
+    private static double getAverageSalary(Employee[] employees){
+        int salarySum = getSalarySum(employees);
+        return (double) salarySum / employees.length;
+    }
+    private static void printFullNames(Employee[] employees) {
+        for (Employee employee : employees) {
+            System.out.println(employee.getFullName());
+
+        }
+    }
+
 }
+
